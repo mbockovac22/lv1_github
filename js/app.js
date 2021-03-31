278e47060986b1a861a78b91b73d0d048dc7137a
unknown <marcel.bockovac@vsmti.hr>
2021-03-31 09:38:15 +0200

    Added delete button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+/*
 
 var btn = document.getElementById("sendButton");
 
@@ -6,6 +6,13 @@ btn.addEventListener("click", function() {
 	$(table).find('tbody').insert("<tr><td>Neki Red</td><tr>");
 
 }, false);
+*/
+
+function Obrisi(btn){
+	 var row = btn.parentNode.parentNode;
+  	row.parentNode.removeChild(row);
+}
+
 
 function Dodaj(){
 	var table = document.getElementById("mainTable");
@@ -17,8 +24,10 @@ function Dodaj(){
 	var cell1 = row.insertCell(0);
 	var cell2 = row.insertCell(1);
 	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
 
 cell1.innerHTML=input1;
 cell2.innerHTML=input2;
 cell3.innerHTML=input3;
+cell4.innerHTML="<button type='button' onclick='Obrisi(this)' id='obrisiButton'>Obrisi</button>";
 }
